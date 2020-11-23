e827146cde8988889228c48d42a475a76753046f
Ekansh Mahendru <ekanshmahendru@gmail.com>
2020-11-23 20:37:22 +0530

    Update create_video.py

--- create_video.py
+++ create_video.py
@@ -6,7 +6,7 @@ frameSize = (640,480)
 
 out = cv2.VideoWriter('output_video.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 10, frameSize)
 
-for filename in range(len(glob.glob('*'))):
+for filename in range(len(glob.glob('gif/*'))):
     print(filename)
     img = cv2.imread("gif/"+str(filename)+".png")
     out.write(img)
